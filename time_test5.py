eeaeab42165ee5deddb425481a05ee4ac12b4292
WebDev <js.develop.com@gmail.com>
2018-06-05 21:03:29 +0300

    Add Task one report

--- time_test5.py
+++ time_test5.py
@@ -2,23 +2,28 @@
 # Python скрипт для тестирования времени загрузки в NumPy & Pandas данных с информации об основных расходах
 # электрической энергии домохозяйствами, собранных в течение 47 месяцев (12.2006 — 11.2010)
 
-import timeit
+# 1)Выбрать все домохозяйства, в которых общая активная потребляемая мощность превышает 5 кВт.
+# **numpy array**, так и **dataframe**, проанализировать временные затраты на выполнение процедур,
+# сделать выводы относительно ситуаций, в которых имеет смысл отдать предпочтение той или иной структуре данных.
+# оформить отчетом с указанным временем выполнения и оценке по 5-балльной шкале удобства выполнения операций отбора)
 
-# import pandas as pd
-# import numpy as np
-# from pandas_dataframe5 import read_csv_file_pd
-# from pandas_dataframe5 import filename2
+# Отчет:
+# Время выполнения функции Pandas read_csv_file_pd() для файла household_power_consumption.csv: 10 - 12 секунд (5 балов)
+# Время выполнения функции NumPy  read_csv_file_np() для файла household_power_consumption.csv: 54 - 62 секунды (2 бала)
+# Предпочтение -> Pandas -> время выполнения операций отбора, экспортируемые отчеты более читабельные
+
+import timeit
 
 filename2 = 'household_power_consumption.csv'
 
-print 'Время выполнения функции read_csv_file_pd() для файла filename2=' + filename2
+print '\n' + 'Время выполнения функции (Pandas) read_csv_file_pd() для файла filename2=' + filename2
 print timeit.repeat("read_csv_file_pd(filename2)",
                     setup="from pandas_dataframe5 import read_csv_file_pd, filename2",
                     number=1,
                     repeat=3
                     )
 
-print 'Время выполнения функции read_csv_file_np() для файла filename2=' + filename2
+print '\n' + 'Время выполнения функции (NumPy) read_csv_file_np() для файла filename2=' + filename2
 print timeit.repeat("read_csv_file_np(filename2)",
                     setup="from numpy_dataframe5 import read_csv_file_np, filename2",
                     number=1,
